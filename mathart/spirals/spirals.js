ab79e26fc04e5459f520aecda1efa9474d57924c
Massimiliano Poletto <max.poletto@gmail.com>
2021-01-25 09:40:33 +0100

    Colors as a function of position in spiral.

--- mathart/spirals/spirals.js
+++ mathart/spirals/spirals.js
@@ -11,22 +11,65 @@ function fix_dpi() {
 }
 
 var points, angle;
+const colors = [];
+
+function init_colors() {
+    colors.length = 256*6;
+    /*
+        https://stackoverflow.com/questions/29229713/iterating-over-rgb-continuously
+        Start from 255 0 0, then count up g to 255 255 0,
+        then count down red to 0 255 0,
+        then count up blue to 0 255 255,
+        then count down green to 0 0 255,
+        then count up red to 255 0 255,
+        then count down blue to 255 0 0.
+    */    
+    var i = 0;
+    for (var j = 0; j <= 255; j++) {
+        colors[i] = 'rgb(255,' + j + ',0)';
+        i++;
+    }
+    for (var j = 255; j >= 0; j--) {
+        colors[i] = 'rgb(' + j + ',255,0)';
+        i++;
+    }
+    for (var j = 0; j <= 255; j++) {
+        colors[i] = 'rgb(0,255,' + j + ')';
+        i++;
+    }
+    for (var j = 255; j >= 0; j--) {
+        colors[i] = 'rgb(0,' + j + ',255)';
+        i++;
+    }
+    for (var j = 0; j <= 255; j++) {
+        colors[i] = 'rgb(' + j + ',0,255)';
+        i++;
+    }
+    for (var j = 255; j >= 0; j--) {
+        colors[i] = 'rgb(255,0,' + j + ')';
+        i++;
+    }
+}
+
 function draw() {
     var w = canvas.width, h = canvas.height;
     ctx.lineWidth = 1;
     ctx.strokeStyle = 'black';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var x = w/2, y = h/2;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
     for (i = 1; i < points; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = colors[i % colors.length];
+        var j = i - 1;
+        ctx.moveTo(x + j*Math.sin(j*angle), y + j*Math.cos(j*angle));
         ctx.lineTo(x + i*Math.sin(i*angle), y + i*Math.cos(i*angle));
+        ctx.stroke();
     }
-    ctx.stroke();
 }
 
 function init() {
     fix_dpi();
+    init_colors();
     var a_in = document.getElementById('angle');
     var p_in = document.getElementById('points');
     var a_out = document.getElementById('angle_span');
